1806a178c2c4c1e430bc65386d7e5f6de47e5706
Jason Messer <jasoncmesser@gmail.com>
2016-10-14 16:01:07 -0700

    player sprite bg transparent

--- main.cpp
+++ main.cpp
@@ -4,49 +4,22 @@
 
 #include <SFML/Graphics.hpp>
 
-void initShape(sf::RectangleShape &, sf::Vector2f const &, sf::Color const&);
-
-void initShape(sf::RectangleShape &shape, sf::Vector2f const &pos, sf::Color const& color)
-{
-	shape.setFillColor(color);
-	shape.setPosition(pos);
-	shape.setOrigin(shape.getSize() * 0.5f); // center of shape
-}
-
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "cliping texture on convex surface");
+    sf::RenderWindow window(sf::VideoMode(800, 600), "sprite is here");
     window.setFramerateLimit(60);
 
-    sf::Texture texture;
-    texture.loadFromFile("iso_tile1.png");
-    sf::Vector2u textureSize = texture.getSize();
-    texture.setRepeated(true);
+    sf::Texture bgTexture;
+    bgTexture.loadFromFile("iso_tile1.png");
+    bgTexture.setRepeated(true);
 
-//    float textureW = static_cast<float>(textureSize.x);
-//    float textureH = static_cast<float>(textureSize.y);
+    sf::RectangleShape background(sf::Vector2f(800, 600));
+    background.setTextureRect(sf::IntRect(0, 0, 800, 600));
+    background.setTexture(&bgTexture);
 
-//    sf::RectangleShape rectangleShape(sf::Vector2f(textureW * 3, textureH * 2));
-//    rectangleShape.setTextureRect(sf::IntRect(0, 0, textureW * 3, textureH * 2));
-
-    sf::RectangleShape rectangleShape(sf::Vector2f(800, 600));
-    rectangleShape.setTextureRect(sf::IntRect(0, 0, 800, 600));
-    rectangleShape.setTexture(&texture);
-
-    sf::Texture shapeTexture;
-    shapeTexture.loadFromFile("80x80@.png");
-
-	sf::ConvexShape shape(5);
-	shape.setPoint(0, sf::Vector2f(0, 0));
-	shape.setPoint(1, sf::Vector2f(200, 0));
-	shape.setPoint(2, sf::Vector2f(180, 120));
-	shape.setPoint(3, sf::Vector2f(100, 200));
-	shape.setPoint(4, sf::Vector2f(20, 120));
-
-	shape.setTexture(&shapeTexture);
-	shape.setOutlineThickness(2);
-	shape.setOutlineColor(sf::Color::Red);
-	shape.move(20, 20);
+    sf::Texture playerTexture;
+    playerTexture.loadFromFile("80x80@.png");
+    sf::Sprite playerSprite(playerTexture);
 
 	while (window.isOpen())
 	{
@@ -76,8 +49,8 @@ int main()
         window.clear(sf::Color::Black);
 
         // draw all the objects
-        window.draw(rectangleShape);
-        window.draw(shape);
+        window.draw(background);
+        window.draw(playerSprite);
 
         window.display();
 	}
